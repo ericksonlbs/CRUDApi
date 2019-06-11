5463f505b2c54950faa1354dd1a9a36bd30d13f9
Erickson Lima <erickson.lbsilva@sp.senac.br>
2019-06-10 21:34:36 -0300

    .

--- Startup.cs
+++ Startup.cs
@@ -37,7 +37,7 @@ namespace CRUDApi
 
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v2", new Info { Title = "TI Senac - São Miguel Paulista - API de Testes - Cursos de TI.", Version = "v1" });
+                c.SwaggerDoc("v2", new Info { Title = "TI Senac - São Miguel Paulista - API de Testes - Cursos de TI.", Version = "v2" });
             });
         }
 
