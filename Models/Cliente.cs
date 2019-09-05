890147898c801eb94a049cd4b5295d63320af8fd
Erickson Lima <erickson.lbsilva@sp.senac.br>
2019-09-04 22:11:29 -0300

    .

--- Models/Cliente.cs
+++ Models/Cliente.cs
@@ -13,5 +13,8 @@ namespace CRUDApi.Models
     public class Endereco{
         public string Estado { get; set; }
         public string Cidade { get; set; }
+        public string Complemento { get; set; }
+        public string Logradouro { get; set; }
+        public string Numero { get; set; }
     }
 }
